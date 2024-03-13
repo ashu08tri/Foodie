3a6692551c2480cf779f4eb8edc9071b89808870
Ashutosh Tripathi <63796943+ashu08tri@users.noreply.github.com>
2024-03-13 22:20:10 +0530

    Update page.jsx

--- src/app/products/[productId]/page.jsx
+++ src/app/products/[productId]/page.jsx
@@ -2,11 +2,9 @@ import {Grid,Typography,Box} from '@mui/material';
 import Image from 'next/image';
 import Price from '@/components/Price';
 
-const {NEXT_PUBLIC_HOST_URL} = process.env;
-
 
 const getData = async (id) => {
-  let res = await fetch(`${NEXT_PUBLIC_HOST_URL}/api/singleProduct/` + id)
+  let res = await fetch(`${process.env.NEXTAUTH_URL}/api/singleProduct/` + id)
   try{
     res = await res.json();
     return res.result; 
