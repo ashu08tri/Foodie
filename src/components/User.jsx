aa5666633c49e4c68e84c7a487e1f69ee70805cf
Ashutosh Tripathi <63796943+ashu08tri@users.noreply.github.com>
2024-03-15 18:21:09 +0530

    Update User.jsx

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -2,22 +2,16 @@
 import Link from 'next/link';
 import { Typography } from '@mui/material';
 import { signOut, useSession } from "next-auth/react";
-import { redirect } from 'next/navigation';
 
 
 function User() {
   const { status } = useSession();
 
-   const logOut = () => {
-    redirect('/')
-    signOut();
-  }
-
   return (
     <>
       {status === 'authenticated' ? <>
         <Link href='/order' style={{color: "#f95959"}}><Typography>ORDERS</Typography></Link>
-        <span onClick={logOut} style={{color: "#f95959", cursor: 'pointer'}}><Typography>LOGOUT</Typography></span>
+        <span onClick={() => signOut()} style={{color: "#f95959", cursor: 'pointer'}}><Typography>LOGOUT</Typography></span>
       </> : <Link href='/login' style={{color: "#f95959"}}><Typography>LOGIN</Typography></Link>}
     </>
   )
